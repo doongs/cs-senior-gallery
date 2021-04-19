0ebacb663a751096c3aaa6ee7a4c6cee7c145107
Gabriel Doon <doongs@dukes.jmu.edu>
2021-04-19 12:51:27 -0400

    json update

--- scripts.js
+++ scripts.js
@@ -1,169 +1,4 @@
-let results = [
-  {
-    timestamp: "10/13/2020 17:43:24",
-    name: "Brad Ridder",
-    eid: "ridderab",
-    minors: "Telecommunications",
-    roles: "CS Ambassador, Cyber Defense, Forensics",
-    advice:
-      "Meet as many people as you can and don’t be afraid to take advantage of opportunities that will get you more involved with the department! One of the many things that makes our department so special is every. single. person. in it. You would be amazed at the stories you will hear in our small CS community alone. As mentioned, this includes getting involved! Getting involved with our CS Ambassador program as a community college transfer student started as an opportunity to share my passion for CS and is now wrapping up as not only that but a family gained. The connections you make today in your college experience are those that you will have the rest of your life - don’t ever be afraid to make them.",
-    memories:
-      "One of the memories that stands out to me was my first semester starting at JMU as a college junior and was at a particularly low moment in my life. During the last bit of the semester, my grandfather passed away and me and my family took it really hard. While this could’ve been a breaking point for my academics after a whole semester, my CS professors lended their hands in making sure that didn’t happen. I have never in my life been so positively overwhelmed with the amount of support I received during that time and will be something I cherish for the rest of my life. When I say JMU CS has become a family to me, I mean it in every sense of the word. Regardless of where we come from or  where we are in our life, our CS department has a place for all of us.",
-    plans: "",
-    extra:
-      "I’d like to personally thank each and every one of the CS faculty and staff for making my experience with the department so special. Being so welcomed, even as a junior transfer from community college, says something about how special JMU is. I am fully prepared to take my many skills learned during my time within the department into the real world, and I truly feel as though it says a lot about what we have. Oh, and one last thing: GO DUKES!",
-  },
-  {
-    timestamp: "10/15/2020 13:20:31",
-    name: "Paul Bailey",
-    eid: "baileypj",
-    minors: "Computer Information Systems",
-    roles: "CS Ambassador",
-    advice:
-      "Engage as much as possible with Computer Science students and faculty! There are so many opportunities for friendship, knowledge, mentorship, research, and careers. Try out the many clubs we have and communicate often with our professors to learn about additional ways to challenge yourself with Computer Science. It may take longer than 4 years to find out what your passion in CS is, so if you work hard to find it early on you can really help yourself out!",
-    memories:
-      "My favorite memories with the CS department were definitely when I found myself up late working on programming assignments in the computer labs. I met so many of my best friends from struggling through code together. The lab was also a fun place to hang out between classes and meet peers in the department. Outside of school, going on camping trips with my CS friends was always a great way to de-stress and have fun away from school-work.",
-    plans:
-      "I have accepted an offer from Fannie Mae in Northern Virginia as a technical engineer. It will start summer 2021 and is only 10 minutes away from my home!",
-    extra:
-      "Computer Science can be very challenging sometimes, but it important to remember that you don't have to learn it on your own. Professors are always there to help and give guidance. 4 years go by fast, make the most of it!",
-  },
-  {
-    timestamp: "10/21/2020 12:12:41",
-    name: "Tyler van Burk",
-    eid: "vanburtj",
-    minors: "Philosophy",
-    roles: "ACM/Coders Anonymous, PlayMU",
-    advice:
-      "Ask Questions, Go to Office Hours, but most of all be open to new experiences or opportunities and be curious. There's always so much going on and different ways to get involved, and a lot of amazing people to meet!",
-    memories:
-      "I loved how welcoming the environment was my first year at JMU. College was a new and scary place, but joining Coders Anonymous was such an amazing decision I made, as well as getting involved with the intramural teams or clubs really helped me find my groove in the CS department.",
-    plans:
-      "I really want to explore technical consulting or analytics since I love analyzing new applications and working with clients. I may also explore some of my own talents in game or app design, and maybe even spike an interest in robotics or AI. There's so many new technologies it's hard to ever get bored in this industry.",
-    extra:
-      "I can confidently say JMU's CS program is a lot more involving and different than my friends in other programs at other schools have said. I have loved my time here and wish I could've been here longer, make the most of it, for sure.",
-  },
-  {
-    timestamp: "10/21/2020 23:38:42",
-    name: "Matthew Foley",
-    eid: "foley2mt",
-    minors: "Studio Art",
-    roles: "",
-    advice:
-      "It's never too early to begin developing and strengthening your relationships with your professors, especially as the CS faculty as a whole is comprised of fantastic individuals. They're here for you to succeed.",
-    memories:
-      "Spending freshman and sophomore year in the Linux lab until way after dark bouncing ideas off of each other and collectively persevering to finish our programming assignments on time.",
-    plans:
-      "I plan to immediately transition into working as a full time software engineer, and am considering pursuing a master's degree in the future.",
-    extra: "",
-  },
-  {
-    timestamp: "10/23/2020 11:13:38",
-    name: "Michael Chladon",
-    eid: "chladomj",
-    minors: "Computer Information Systems",
-    roles: "",
-    advice:
-      "Go to office hours and get to know the people in the department! There are so many wonderful faculty members with great advice and experiences. They will not only help you with your course work, but also help you grow as an individual.",
-    memories: "",
-    plans:
-      "I will be working as a Software Engineer at Brightspot in Reston, VA!",
-    extra: "",
-  },
-  {
-    timestamp: "10/24/2020 13:17:50",
-    name: "Elijah Senegar",
-    eid: "senegaej",
-    minors: "",
-    roles: "PlayMU",
-    advice: "Join the clubs!!!",
-    memories: "",
-    plans: "",
-    extra: "",
-  },
-  {
-    timestamp: "10/25/2020 18:11:21",
-    name: "Jedidiah Keplinger",
-    eid: "keplinjr",
-    minors: "Mathematics",
-    roles: "",
-    advice:
-      "I think that being intentional about truly engaging with your coursework, peers, faculty members, and/or possibility helping on a research project will give you a tremendous learning experience. As a CS student you’ll be here to learn, and you’re not expected to immediately know everything. I think to get the most from this department, simply give it your best effort, be patient with yourself as you learn, and your efforts will be reciprocated.",
-    memories:
-      "One of my favorite memories involves the CS student room in the department hall. Jokingly, this room was named: “Dr. Bower’s Auxiliary Office.” The nice thing about this room was getting to interact with other students, work on problems together, and occasionally talk with a faculty member that dropped by. However, one of my favorite memories was when myself and other students began making the room our own. We decided the florescent lighting was a bit draining, and so I offered to bring in a standing lamp from home. So, there I was. A junior CS major in early hours of a windy winter morning carrying a 5-ft tall lamp across the parking lot by Rose Library to the ISAT building. I can’t help but laugh at how comical the moment was. But the CS department really made me feel at home.",
-    plans:
-      "I plan to join the work force for the time being. Eventually, I would like to work towards a master’s degree, or if I’ve had enough coffee, I might consider pursuing a PhD in Computer Science as I would like to end up teaching the subject.",
-    extra:
-      "If I could share advice with my younger self, I would encourage curiosity when it comes to learning. I think it’s easy to fall into the mentality of learning for the sake of maintaining grades, which results in memorization over true learning and understanding. My experience in undergrad has taught me that learning isn’t about memorizing facts at all (well, you’ll have to memorize some of course). But learning is really about how to think, analyze, and (in CS/math at least) problem solve. So, be curious. Don’t settle for factualization. In other words, don’t stop at simply knowing that (binary) 0010 = 2 (decimal), ask yourself: “Why is this true?” and follow the trail. I learned and retained more thinking this way rather than trying to memorize factual elements.",
-  },
-  {
-    timestamp: "10/25/2020 19:12:11",
-    name: "Emma Macaluso",
-    eid: "macaluec",
-    minors: "Biology, Creative Writing",
-    roles: "CS TA, Upsilon Pi Epsilon",
-    advice:
-      "Say yes more than you say no and try things that put you out of your comfort zone!  Talk to the people sitting next to you in class and the people in your groups. Everyone is struggling and if you are open with the people around you, it's easier to make it through. Some of my very good friends are close to me now because we struggled through 261 and 240 projects together. Don't be afraid to ask for help!",
-    memories:
-      "I have so many! I'll never forget volunteering at EYH and DIGITAL and seeing young girls try programming the first time. One of my favorite things from all of college was camping with CS people. Being in the lab between classes or during TA hours. Playing D&D! Going to chili's! Sitting in Dr. Weikle's red chair. Debating if foods are soups or sandwiches with Dr. Bernstein. Writing on the chalkboard room. So many memories, I could go on forever.",
-    plans: "",
-    extra:
-      "I switched my major a couple times before finding CS, and I'm so happy I landed here. This is a department that values its students and their success. The programs in it help students be better and bigger thinkers. The faculty are inspiring, caring, and genuine. I will miss this department and everyone in it very much next year.",
-  },
-  {
-    timestamp: "10/26/2020 12:46:42",
-    name: "Xinzhe He",
-    eid: "hexx",
-    minors: "Biology",
-    roles: "",
-    advice:
-      "Participate in the Unix Lab to better understand the development environment required as a professional programmer in the future. Participate in the organization or activities in the department as much as possible, and try to be a TA.",
-    memories:
-      "Due to the pandemic, I couldn't normally team up with friends to develop late at night in the computer labs in the last few months before graduation, as in the past few years. That may be the experience I miss most now.",
-    plans: "",
-    extra: "",
-  },
-  {
-    timestamp: "10/26/2020 15:30:33",
-    name: "Hannah Ripley",
-    eid: "ripleyhk",
-    minors: "",
-    roles: "CS TA, Upsilon Pi Epsilon, Women in Technology",
-    advice: "Get involved! We're really social for a bunch of programmers!",
-    memories:
-      "I miss bonding with everyone during TA hours! We were always laughing and learning!",
-    plans:
-      "I'll be working as a software engineer at Johns Hopkins University Applied Physics Lab!",
-    extra: "",
-  },
-  {
-    timestamp: "10/26/2020 17:50:09",
-    name: "Nicole Stock",
-    eid: "stockne",
-    minors:
-      "Mathematics (Double Major) and Honors Studies Minor",
-    roles: "Upsilon Pi Epsilon, Women in Technology",
-    advice:
-      "Definitely try to get involved in clubs and organizations both related to CS and not! Get to know your classmates: going to class is a lot more fun with friends and studying becomes much easier if you have people to work with. Introduce yourself to your professors and get to know them!",
-    memories:
-      "Being involved in the Women In Technology organization has been one of the highlights of my JMU CS career! It has given me a supportive community of intelligent women with similar interests and career goals and introduced me to new friends.",
-    plans:
-      "I plan to pursue a masters degree in Computer Science and/or begin working full-time as a software engineer.",
-    extra: "",
-  },
-  {
-    timestamp: "10/29/2020 13:42:57",
-    name: "Ryan Maring",
-    eid: "maringrm",
-    minors: "Mathematics",
-    roles: "",
-    advice: "Don't be afraid to ask for help and go to TA hours!",
-    memories: "159 PA 4 the asteroids game!",
-    plans: "Work in Arlington",
-    extra: "",
-  },
-];
-
+let results;
 function createCard(index) {
   let student = results[index];
   let eid = student.eid;
@@ -173,9 +8,9 @@ function createCard(index) {
   let card = document.createElement("div");
   card.classList.add("col-md-4", "pb-4");
 
-  let body =``;
+  let body = ``;
 
-  if(student.minors != "") {
+  if (student.minors != "") {
     top = `Minors: ${student.minors}`;
     bottom = `${student.roles}`;
   } else {
@@ -336,36 +171,43 @@ function setModalText(index) {
 
 function doesFileExist(variable) {
   var image = new Image();
-    image.src = variable;
-    if (image.width == 0) {
-       return false;
-    } else {
-       return true;
-    }
+  image.src = variable;
+  if (image.width == 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 (function () {
+  fetch("responses.json")
+    .then((response) => response.json())
+    .then((data) => processResults(data.responses));
+})();
+
+function processResults(res) {
+  results = res;
   results.sort((a, b) => {
     let fa = a.eid.toLowerCase(),
-        fb = b.eid.toLowerCase();
-
+      fb = b.eid.toLowerCase();
     if (fa < fb) {
-        return -1;
+      return -1;
     }
     if (fa > fb) {
-        return 1;
+      return 1;
     }
     return 0;
-});
-  //alert(results.length);
+  });
+
   let cards = document.querySelector("#cards");
-  console.log(Math.floor(results.length / 3));
-  for (let i = 0; i < Math.floor(results.length / 3); i++) {
+
+  for (let i = 0; i < Math.ceil(results.length / 3); i++) {
     let row = document.createElement("div");
     row.classList.add("row");
     row.id = `row${i}`;
     cards.appendChild(row);
   }
+
   let counter = -1;
   for (let i = 0; i < results.length; i++) {
     if (i % 3 == 0) {
@@ -373,6 +215,5 @@ function doesFileExist(variable) {
     }
     document.querySelector(`#row${counter}`).appendChild(createCard(i));
   }
-  $('.carousel').carousel();
-  //location.reload();
-})();
+  $(".carousel").carousel();
+}
